cd9e944c8cc6fefcbe8a26a20f1bae84e8af7ea8
Geison <geisonshorts@gmail.com>
2025-11-11 01:08:55 -0300

    Criando funções e exportando e importando

---
+++ src/calculadora.js
@@ -0,0 +1,11 @@
+export function somarDoisNumeros(valor1, valor2) {
+    const resultado = valor1 + valor2;
+    return resultado;
+}
+
+function calcularSomaDeDoisNumeros(valor1, valor2) {
+    const resultadoSomaDeDoisValores = somarDoisNumeros(valor1, valor2)
+    const resultadoDaMediaDeDoisNumeros = resultadoSomaDeDoisValores / 2
+    return resultadoDaMediaDeDoisNumeros
+}
+
